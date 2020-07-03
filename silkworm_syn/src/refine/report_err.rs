61aacb834d5953b70069318db4e61e92e548c7f2
toasteater <48371905+toasteater@users.noreply.github.com>
2020-07-03 07:26:17 +0000

    Attempt to catch more refine bugs

--- silkworm_syn/src/refine/report_err.rs
+++ silkworm_syn/src/refine/report_err.rs
@@ -4,7 +4,7 @@ use crate::ast::visit::Visit;
 use crate::ast::*;
 use crate::parse::ParseCtx;
 
-/// Validation transform that reports remaining `Err` AST nodes that might have remained after prior transforms passes.
+/// Validation transform that reports remaining invalid AST nodes that might have remained after prior transforms passes.
 pub struct ReportErr<'a> {
     errors: &'a ErrorCtx,
 }
@@ -42,17 +42,62 @@ impl<'a, 'ast> Visit<'ast> for ReportErr<'a> {
             StmtKind::Text(str_body) => self.visit_str_body(str_body),
             StmtKind::Command(command) => self.visit_command(command),
             StmtKind::Flow(flow) => self.visit_flow(flow),
-            StmtKind::ShortcutOption(shortcut_option) => {
-                self.visit_shortcut_option(shortcut_option)
-            }
-            StmtKind::Block(block) => self.visit_block(block),
             StmtKind::If(if_stmt) => self.visit_if_stmt(if_stmt),
             StmtKind::Shortcuts(shortcuts_stmt) => self.visit_shortcuts_stmt(shortcuts_stmt),
+
+            StmtKind::Block(_) => {
+                self.errors
+                    .bug("remaining raw Block statement")
+                    .span(stmt_body.span);
+            }
+            StmtKind::ShortcutOption(_) => {
+                self.errors
+                    .bug("remaining raw ShortcutOption statement")
+                    .span(stmt_body.span);
+            }
             StmtKind::Err => {
                 self.errors
                     .bug("remaining Err statement")
                     .span(stmt_body.span);
             }
+        }
+    }
+
+    fn visit_command(&mut self, command: &'ast Command) {
+        match &command.kind {
+            CommandKind::Set(var, value) => {
+                self.visit_var(var);
+                self.visit_expr(value);
+            }
+            CommandKind::Call(expr) => self.visit_expr(expr),
+            CommandKind::Return(Some(expr)) => self.visit_expr(expr),
+            CommandKind::Return(None) => {}
+            CommandKind::Custom(str_body) => self.visit_str_body(str_body),
+
+            CommandKind::If(_)
+            | CommandKind::ElseIf(_)
+            | CommandKind::Else
+            | CommandKind::EndIf => {
+                self.errors.bug("remaining if-command").span(command.span);
+            }
+        }
+    }
+
+    fn visit_stmt(&mut self, stmt: &'ast Stmt) {
+        for pragma in &stmt.pragmas {
+            self.visit_pragma(pragma);
+        }
+
+        self.visit_stmt_body(&stmt.body);
+
+        if let Some(command) = stmt.decorator_command.as_ref() {
+            self.errors
+                .bug("remaining decorator command")
+                .span(command.span);
+        }
+
+        for hashtag in &stmt.hashtags {
+            self.visit_hashtag(hashtag);
         }
     }
 }
